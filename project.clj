812bd24a77ecc22a3dcc2561fa179f8ce3d46a30
Filip Bielejec <fbielejec@gmail.com>
2020-02-03 15:56:11 +0100

    add support for eip-55 (#1)
    * add support for eip-55
    
    * add CircleCi integration

--- project.clj
+++ project.clj
@@ -1,36 +1,43 @@
-(defproject district0x/district-ui-web3-accounts "1.0.6"
+(defproject district0x/district-ui-web3-accounts "1.0.7-SNAPSHOT"
   :description "district UI module for handling web3 accounts"
   :url "https://github.com/district0x/district-ui-web3-accounts"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[cljs-web3 "0.19.0-0-9"]
-                 [day8.re-frame/forward-events-fx "0.0.5"]
+  :dependencies [[cljs-web3 "0.19.0-0-10"]
+                 [day8.re-frame/forward-events-fx "0.0.6"]
                  [district0x/district-ui-web3 "1.3.2"]
                  [district0x/district-ui-window-focus "1.0.0"]
+                 [district0x/eip55 "0.0.1"]
                  [district0x/re-frame-interval-fx "1.0.2"]
                  [district0x/re-frame-spec-interceptors "1.0.1"]
                  [district0x.re-frame/web3-fx "1.0.5"]
-                 [mount "0.1.11"]
-                 [org.clojure/clojurescript "1.9.946"]
-                 [re-frame "0.10.2"]]
+                 [mount "0.1.16"]
+                 [org.clojure/clojurescript "1.10.597"]
+                 [re-frame "0.11.0"]]
 
   :doo {:paths {:karma "./node_modules/karma/bin/karma"}}
 
-  :npm {:devDependencies [[karma "1.7.1"]
-                          [karma-chrome-launcher "2.2.0"]
-                          [karma-cli "1.0.1"]
-                          [karma-cljs-test "0.1.0"]]}
-
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
-                                  [com.cemerick/piggieback "0.2.2"]
-                                  [org.clojure/tools.nrepl "0.2.13"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
                                   [day8.re-frame/test "0.1.5"]]
+                   :source-paths ["src" "test"]
                    :plugins [[lein-cljsbuild "1.1.7"]
-                             [lein-doo "0.1.8"]
-                             [lein-npm "0.6.2"]]}}
+                             [lein-doo "0.1.8"]]}}
 
-  :cljsbuild {:builds [{:id "tests"
+  :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]
+                        ["releases"  {:url "https://clojars.org/repo"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]]
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["deploy"]]
+
+  :cljsbuild {:builds [{:id "browser-tests"
                         :source-paths ["src" "test"]
                         :compiler {:output-to "tests-output/tests.js"
                                    :output-dir "tests-output"
--- src/district/ui/web3_accounts/events.cljs
+++ src/district/ui/web3_accounts/events.cljs
@@ -1,126 +1,118 @@
 (ns district.ui.web3-accounts.events
-  (:require
-    [cljs-web3.core :as web3]
-    [cljs-web3.eth :as web3-eth]
-    [cljs.spec.alpha :as s]
-    [day8.re-frame.forward-events-fx]
-    [district.ui.web3-accounts.effects :as effects]
-    [district.ui.web3-accounts.queries :as queries]
-    [district.ui.web3.events :as web3-events]
-    [district.ui.web3.queries :as web3-queries]
-    [district.ui.window-focus.queries :as window-focus-queries]
-    [district0x.re-frame.interval-fx]
-    [district0x.re-frame.spec-interceptors :refer [validate-first-arg]]
-    [district0x.re-frame.web3-fx]
-    [re-frame.core :refer [reg-event-fx trim-v]]))
+  (:require [cljs-web3.core :as web3]
+            [cljs-web3.eth :as web3-eth]
+            [cljs.spec.alpha :as s]
+            [day8.re-frame.forward-events-fx]
+            [district.ui.web3-accounts.effects :as effects]
+            [district.ui.web3-accounts.queries :as queries]
+            [district.ui.web3.events :as web3-events]
+            [district.ui.web3.queries :as web3-queries]
+            [district.ui.window-focus.queries :as window-focus-queries]
+            [district0x.re-frame.interval-fx]
+            [district0x.re-frame.spec-interceptors :as spec-interceptors]
+            [district0x.re-frame.web3-fx]
+            [eip55.core :as eip55]
+            [re-frame.core :as re-frame]))
 
-(def interceptors [trim-v])
+(def interceptors [re-frame/trim-v])
 
 (s/def ::account web3/address?)
 (s/def ::accounts (s/coll-of ::account))
 
-(reg-event-fx
-  ::start
-  interceptors
-  (fn [{:keys [:db]} [{:keys [:disable-polling? :polling-interval-ms :disable-loading-at-start?]
-                       :or {polling-interval-ms 4000}
-                       :as opts}]]
-    (merge
-      {:db (queries/assoc-accounts db [])}
-      (when-not disable-loading-at-start?
-        {:forward-events {:register ::load-accounts
-                          :events #{::web3-events/web3-created}
-                          :dispatch-to [::load-accounts opts]}})
-      (when (and (not disable-polling?)
-                 (not disable-loading-at-start?))
-        (if (some-> js/window (aget "ethereum") (aget "on"))
-          {::effects/watch-accounts {:on-change [::set-accounts]}}
-          {:dispatch-interval {:dispatch [::poll-accounts opts]
-                               :id ::poll-accounts
-                               :ms polling-interval-ms}})))))
+(defn reg-opts-cofx [opts]
+  (re-frame/reg-cofx
+   :opts
+   (fn [coeffects _]
+     (assoc coeffects :opts opts))))
 
+(re-frame/reg-event-fx
+ ::start
+ interceptors
+ (fn [{:keys [:db]} [{:keys [:disable-polling? :polling-interval-ms :disable-loading-at-start?]
+                      :or {polling-interval-ms 4000}
+                      :as opts}]]
+   (merge
+    {:db (queries/assoc-accounts db [])}
+    (when-not disable-loading-at-start?
+      {:forward-events {:register ::load-accounts
+                        :events #{::web3-events/web3-created}
+                        :dispatch-to [::load-accounts opts]}})
+    (when (and (not disable-polling?)
+               (not disable-loading-at-start?))
+      (if (some-> js/window (aget "ethereum") (aget "on"))
+        {::effects/watch-accounts {:on-change [::set-accounts]}}
+        {:dispatch-interval {:dispatch [::poll-accounts opts]
+                             :id ::poll-accounts
+                             :ms polling-interval-ms}})))))
 
-(reg-event-fx
-  ::poll-accounts
-  interceptors
-  (fn [{:keys [:db]} [opts]]
-    (merge
-      (when (and (window-focus-queries/focused? db)         ;; Important perf optimisation
-                 (web3-queries/web3 db))
-        {:dispatch [::load-accounts opts]})
-      (when (and (web3-queries/web3 db)
-                 (not (web3-queries/web3-injected? db)))
-        {:clear-interval {:id ::poll-accounts}}))))
-
-
-(reg-event-fx
-  ::load-accounts
-  interceptors
-  (fn [{:keys [:db]} [{:keys [:load-injected-accounts-only?]}]]
-    (when-let [web3 (web3-queries/web3 db)]
-      (if (and load-injected-accounts-only?
+(re-frame/reg-event-fx
+ ::poll-accounts
+ interceptors
+ (fn [{:keys [:db]} [opts]]
+   (merge
+    (when (and (window-focus-queries/focused? db)         ;; Important perf optimisation
+               (web3-queries/web3 db))
+      {:dispatch [::load-accounts opts]})
+    (when (and (web3-queries/web3 db)
                (not (web3-queries/web3-injected? db)))
-        {:dispatch [::set-accounts []]}
-        {:web3/call {:web3 web3
-                     :fns [{:fn web3-eth/accounts
-                            :on-success [::set-accounts]
-                            :on-error [::accounts-load-failed]}]}}))))
+      {:clear-interval {:id ::poll-accounts}}))))
 
+(re-frame/reg-event-fx
+ ::load-accounts
+ interceptors
+ (fn [{:keys [:db]} [{:keys [:load-injected-accounts-only?]}]]
+   (when-let [web3 (web3-queries/web3 db)]
+     (if (and load-injected-accounts-only?
+              (not (web3-queries/web3-injected? db)))
+       {:dispatch [::set-accounts []]}
+       {:web3/call {:web3 web3
+                    :fns [{:fn web3-eth/accounts
+                           :on-success [::set-accounts]
+                           :on-error [::accounts-load-failed]}]}}))))
 
-(reg-event-fx
-  ::accounts-load-failed
-  interceptors
-  (fn []
-    {:dispatch [::set-accounts []]}))
+(re-frame/reg-event-fx
+ ::accounts-load-failed
+ interceptors
+ (fn []
+   {:dispatch [::set-accounts []]}))
 
+(re-frame/reg-event-fx
+ ::set-accounts
+ [(re-frame/inject-cofx :opts) interceptors (spec-interceptors/validate-first-arg ::accounts)]
+ (fn [{:keys [:db]
+       {:keys [:eip55?]} :opts} [accounts]]
+   (let [accounts (if eip55? (map eip55/address->checksum accounts) accounts) ;; support for EIP-55, needed ONLY until UI libraries are migrated to web3 1.0 which supports it OOB
+         active-account (if (contains? (set accounts) (queries/active-account db))
+                          (queries/active-account db)
+                          (first accounts))]
+     (merge
+      (when (not= accounts (queries/accounts db))
+        {:db (queries/assoc-accounts db accounts)
+         :dispatch [::accounts-changed {:new accounts :old (queries/accounts db)}]})
+      {:dispatch-n [[::set-active-account active-account]]}))))
 
-(reg-event-fx
-  ::set-accounts
-  [interceptors (validate-first-arg ::accounts)]
-  (fn [{:keys [:db]} [accounts]]
-    (let [active-account (if (contains? (set accounts) (queries/active-account db))
-                           (queries/active-account db)
-                           (first accounts))]
-      (merge
-        (when (not= accounts (queries/accounts db))
-          {:db (queries/assoc-accounts db accounts)
-           :dispatch [::accounts-changed {:new accounts :old (queries/accounts db)}]})
-        {:dispatch-n [[::set-active-account active-account]]}))))
+(re-frame/reg-event-fx
+ ::accounts-changed
+ (constantly nil))
 
+(re-frame/reg-event-fx
+ ::set-active-account
+ [interceptors (spec-interceptors/validate-first-arg (s/nilable ::account))]
+ (fn [{:keys [:db]} [active-account]]
+   (when (not= active-account (queries/active-account db))
+     {:db (queries/assoc-active-account db active-account)
+      :dispatch [::active-account-changed {:new active-account :old (queries/active-account db)}]})))
 
-(reg-event-fx
-  ::accounts-changed
-  (constantly nil))
+(re-frame/reg-event-fx
+ ::active-account-changed
+ (constantly nil))
 
-
-(reg-event-fx
-  ::set-active-account
-  [interceptors (validate-first-arg (s/nilable ::account))]
-  (fn [{:keys [:db]} [active-account]]
-    (when (not= active-account (queries/active-account db))
-      {:db (queries/assoc-active-account db active-account)
-       :dispatch [::active-account-changed {:new active-account :old (queries/active-account db)}]})))
-
-
-(reg-event-fx
-  ::active-account-changed
-  (constantly nil))
-
-
-(reg-event-fx
-  ::stop
-  interceptors
-  (fn [{:keys [:db]} [{:keys [:disable-loading-at-start?]}]]
-    (merge
-      {:db (queries/dissoc-web3-accounts db)
-       :clear-interval {:id ::poll-accounts}}
-      (when-not disable-loading-at-start?
-        {:forward-events {:unregister ::load-accounts}}))))
-
-
-
-
-
-
-
-
+(re-frame/reg-event-fx
+ ::stop
+ interceptors
+ (fn [{:keys [:db]} [{:keys [:disable-loading-at-start?]}]]
+   (merge
+    {:db (queries/dissoc-web3-accounts db)
+     :clear-interval {:id ::poll-accounts}}
+    (when-not disable-loading-at-start?
+      {:forward-events {:unregister ::load-accounts}}))))
